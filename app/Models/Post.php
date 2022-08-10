96ba9c10de833e73df672edfe10e14ab1ec0befa
Aaron Hope <aghopecode@gmail.com>
2022-08-10 15:03:25 -0700

    Collectoin Sorting and Caching Refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,16 +28,18 @@ class Post
   }
   public static function all()
   {
-    return collect(File::files(resource_path("posts")))
-    ->map(fn($file) => YamlFrontMatter::parseFile($file))
-    ->map(fn($document) => new Post(
-      $document->title,
-      $document->excerpt,
-      $document->date,
-      $document->body(),
-      $document->slug,
-    ))
-    ->sortByDesc('date');
+    return cache()->rememberForever('posts.all', function() {
+      return collect(File::files(resource_path("posts")))
+      ->map(fn($file) => YamlFrontMatter::parseFile($file))
+      ->map(fn($document) => new Post(
+        $document->title,
+        $document->excerpt,
+        $document->date,
+        $document->body(),
+        $document->slug,
+      ))
+      ->sortByDesc('date');
+    });
   }
 
   public static function find($slug)
