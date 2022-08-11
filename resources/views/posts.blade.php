2b72b2666ee8904c653f3e6cc4b823ff55fc7d1d
Aaron Hope <aghopecode@gmail.com>
2022-08-10 17:18:39 -0700

    Created layout.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <link rel="stylesheet" href="/app.css">
-  
-  <title>My Blog</title>
+@extends ('layout')
 
-</head>
-<body>
+@section('content')
   @foreach ($posts as $post)
     <article class="{{ $loop->even ? 'mt-6' : ''}}">
       <h1>
@@ -22,5 +12,4 @@
       <div>{{ $post->excerpt }}</div>
     </article>
   @endforeach
-</body>
-</html>
+@endsection
