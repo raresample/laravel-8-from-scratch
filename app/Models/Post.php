b74d784c41683b69068153082f5b63536a9e2cc8
Aaron Hope <aghopecode@gmail.com>
2022-08-19 01:10:04 -0700

    Your First Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'excerpt', 'body', 'slug'];
+    
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -20,6 +20,10 @@
         <h6>
             Published on <?= gmdate("Y-m-d", $post->date); ?>
         </h6>
+        
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
 
         <div>
             <!-- <?= $post->body; ?> -->
