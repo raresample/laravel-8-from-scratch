0b4ade02f7d98bb3864ba05bffb3e68e1eb48790
Aaron Hope <aghopecode@gmail.com>
2022-08-19 14:32:12 -0700

    Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,21 +4,61 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    User::truncate();
+    Category::truncate();
+    Post::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    $user = User::factory()->create();
+
+    $personal = Category::create([
+      'name' => 'Personal',
+      'slug' => 'personal'
+    ]);
+
+    $family = Category::create([
+      'name' => 'Family',
+      'slug' => 'family'
+    ]);
+
+    $work = Category::create([
+      'name' => 'Work',
+      'slug' => 'work'
+    ]);
+
+    Post::create([
+      'user_id' => $user->id,
+      'category_id' => $family->id,
+      'title' => 'My Family Post',
+      'slug' => 'my-first-post',
+      'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+      'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto aliquid assumenda omnis molestias quia numquam perferendis debitis tempore ab totam maiores dolore, quasi ipsa at porro consequatur rem error id!</p>'
+    ]);
+
+    Post::create([
+      'user_id' => $user->id,
+      'category_id' => $work->id,
+      'title' => 'My Work Post',
+      'slug' => 'my-second-post',
+      'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
+      'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto aliquid assumenda omnis molestias quia numquam perferendis debitis tempore ab totam maiores dolore, quasi ipsa at porro consequatur rem error id!</p>'
+    ]);
+
+    // \App\Models\User::factory()->create([
+    //   'name' => 'Test User',
+    //   'email' => 'test@example.com',
+    // ]);
+  }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -22,7 +22,7 @@
         </h6>
         
         <p>
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
 
         <div>
