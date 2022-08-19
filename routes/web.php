85ae437a8de35fd8466f8dad2c0e3f39ed298ab1
Aaron Hope <aghopecode@gmail.com>
2022-08-19 02:53:17 -0700

    Clockwork, and the N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('posts', [
-    'posts' => Post::all()
+    'posts' => Post::with('category')->get()
   ]);
 });
 
