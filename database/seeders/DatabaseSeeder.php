bf4058d9aad3a0c1367fdb5687e2bf07c3071fb6
Aaron Hope <aghopecode@gmail.com>
2022-08-20 00:58:18 -0700

    Turbo Boost with Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,39 +21,12 @@ class DatabaseSeeder extends Seeder
     Category::truncate();
     Post::truncate();
 
-    $user = User::factory()->create();
-
-    $personal = Category::create([
-      'name' => 'Personal',
-      'slug' => 'personal'
+    $user = User::factory()->create([
+      'name' => 'John Doe'
     ]);
 
-    $family = Category::create([
-      'name' => 'Family',
-      'slug' => 'family'
-    ]);
-
-    $work = Category::create([
-      'name' => 'Work',
-      'slug' => 'work'
-    ]);
-
-    Post::create([
-      'user_id' => $user->id,
-      'category_id' => $family->id,
-      'title' => 'My Family Post',
-      'slug' => 'my-first-post',
-      'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-      'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto aliquid assumenda omnis molestias quia numquam perferendis debitis tempore ab totam maiores dolore, quasi ipsa at porro consequatur rem error id!</p>'
-    ]);
-
-    Post::create([
-      'user_id' => $user->id,
-      'category_id' => $work->id,
-      'title' => 'My Work Post',
-      'slug' => 'my-second-post',
-      'excerpt' => '<p>Lorem ipsum dolar sit amet.</p>',
-      'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto aliquid assumenda omnis molestias quia numquam perferendis debitis tempore ab totam maiores dolore, quasi ipsa at porro consequatur rem error id!</p>'
+    Post::factory(5)->create([
+      'user_id' => $user->id
     ]);
 
     // \App\Models\User::factory()->create([
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -12,7 +12,7 @@
         <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
       </p>
 
-      <div>{{ $post->excerpt }}</div>
+      <div>{!! $post->excerpt !!}</div>
     </article>
   @endforeach
 </x-layout>
