4e5fcf0b5fddd7a8725095ef1a289a14495cc3f6
Aaron Hope <aghopecode@gmail.com>
2022-08-22 14:57:42 -0700

    View All Posts by an Author

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
   return view('posts', [
-    'posts' => Post::with('category')->get()
+    'posts' => Post::latest()->with('category', 'author')->get()
   ]);
 });
 
@@ -31,5 +32,11 @@ Route::get("posts/{post:slug}", function (Post $post) {
 Route::get("/categories/{category:slug}", function (Category $category) {
   return view('posts', [
     'posts' => $category->posts
+  ]);
+});
+
+Route::get("/authors/{author:username}", function (User $author) {
+  return view('posts', [
+    'posts' => $author->posts
   ]);
 });
