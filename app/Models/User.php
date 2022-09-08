1fe2b333c1ec8764067b6fa497cf4dc68a96dc2f
Aaron Hope <aghopecode@gmail.com>
2022-09-08 16:41:01 -0700

    Show a Success Message Flash

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function setPasswordAttribute($password)
     {
-        $this->attributes('password') = bcrypt($password);
+        $this->attributes['password'] = bcrypt($password);
     }
 
     public function posts() 
